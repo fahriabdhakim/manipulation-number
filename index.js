0e344640152fe64d082ea29294d721b504799bdd
Fahri Abdul Hakim <fahriabdhakim@yahoo.com>
2018-01-15 13:17:55 +0700

    index.js lvl 0-1

---
+++ index.js
@@ -0,0 +1,25 @@
+// LEVEL 0
+
+var numberA = 10
+let numberB = 099
+const numberC = 0.02
+
+console.log(numberA);
+console.log(numberB);
+console.log(numberC);
+
+
+
+// LEVEL 1
+
+var a = numberA + numberC
+var b = numberB - numberA
+var c = numberC * numberB
+var d = numberA / numberC
+var e = numberB % numberA
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
